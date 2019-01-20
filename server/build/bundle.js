cedc178a617d0526172e44b078286aebae4e78f7
Dean Asadi <eng.de86@gmail.com>
2019-01-20 16:35:00 -0600

    running nodemon & webpack command togather

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -22128,7 +22128,7 @@ var Home = function Home() {
   return _react2.default.createElement(
     "div",
     null,
-    "Hi, I am Home component."
+    "Hi, I am Home component..."
   );
 };
 
--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Hi, I am Home component.</div>;
+  return <div>Hi, I am Home component...</div>;
 };
 
 export default Home;
